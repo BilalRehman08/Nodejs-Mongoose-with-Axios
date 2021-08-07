19e808cd7ec76bbefd2a5ff9256c59c1dc0aa1e7
BilalRehman08 <bilalrehman080808@gmail.com>
2021-08-07 08:37:09 -0700

    first commit

--- web/post.js
+++ web/post.js
@@ -10,6 +10,7 @@ function send() {
     }
     console.log(obj)
 
+
     axios.post(`http://localhost:5000/add`, obj)
         .then(function (response) {
             console.log(response);
@@ -25,10 +26,22 @@ function send() {
 function read() {
     axios.get(`http://localhost:5000/read`)
         .then(function (response) {
-
-
-            console.log(response);
+            // console.log(response);
             const data = response
+            var p = document.getElementById("post")
+            for (var i in data.data) {
+                var a = document.createElement("li")
+                a.innerHTML = `<div class="card" style="width: 18rem;">
+               
+                <div class="card-body">
+                  <h5 class="card-title">${data.data[i].title}</h5>
+                  <p class="card-text">${data.data[i].description}</p>
+                  
+                </div>
+              </div>`
+
+                p.appendChild(a)
+            }
         })
         .catch(function (error) {
             // handle error
@@ -36,3 +49,9 @@ function read() {
         })
 
 }
+
+// var abc = [1, 2, 4, 5, 6];
+// var p = document.getElementById("post")
+// var a = document.createElement("li")
+// a.innerHTML = 
+// p.appendChild(a)
